d3b9b76dcb596506468c74ac3b96c12ab32dd939
audidas <audidas@naver.com>
2021-01-15 15:34:05 +0900

    get to post

--- src/thingdata/thingdata.controller.ts
+++ src/thingdata/thingdata.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Request } from 'express';
 import { ThingdataService } from './thingdata.service';
 
@@ -6,8 +6,8 @@ import { ThingdataService } from './thingdata.service';
 export class ThingdataController {
   constructor(private readonly thingdataService: ThingdataService) {}
 
-  @Get('/')
-  getThingPlugData(req: Request) {
-    this.thingdataService.getThingPlugData(req);
+  @Post('/')
+  getThingPlugData(req: Request, @Body() body) {
+    this.thingdataService.getThingPlugData(req, body);
   }
 }
